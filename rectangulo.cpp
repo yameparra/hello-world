5a22ae891ace663c808d2d05ca89c9cf3c3b800d
yamelparra <morenaparra@icloud.com>
2025-11-10 21:11:07 -0600

    Agregar programa que calcula el área de un rectángulo

---
+++ rectangulo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float base, altura, area;
+    base = 5.0;
+    altura = 3.2;
+    area = base * altura;
+    cout << "El area del rectangulo es: " << area << endl;
+
+    return 0;
+}
